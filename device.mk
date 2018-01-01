9f403f2d35b8c4a711bcec3c33156e1d36739689
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    X00T: power: switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- device.mk
+++ device.mk
@@ -349,10 +349,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.X00T-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
