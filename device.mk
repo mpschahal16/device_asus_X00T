94b9fd81fb61a0455953328ef732ded5b0862a73
Ramakant Singh <ramakant@codeaurora.org>
2019-03-28 11:30:00 +0530

    X00T: rootdir: Add new power script to build
    CRs-Fixed: 2424966
    Change-Id: I90ae8d04befb03820abd7392d15f5b7574735e26

--- device.mk
+++ device.mk
@@ -351,6 +351,7 @@ PRODUCT_PACKAGES += \
     init.btmac.sh \
     init.class_main.sh \
     init.qcom.post_boot.sh \
+    init.qcom.power.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
