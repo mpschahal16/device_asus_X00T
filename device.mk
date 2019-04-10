f5d922e9fb38b754152ac54b6c452cb441807c78
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-10 14:47:17 +0800

    X00T: device: Build services-ext
    Change-Id: I397d9bc624834544a88f4cd7b9dfe9b5e544082b

--- device.mk
+++ device.mk
@@ -145,7 +145,8 @@ PRODUCT_PACKAGES += \
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
-    com.quicinc.cne
+    com.quicinc.cne \
+    services-ext
 
 # Configstore
 PRODUCT_PACKAGES += \
