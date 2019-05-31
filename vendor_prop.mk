58345cf5a303cc0d2fe8592ba5e1d8abdc83f746
SayanAcharya <rowdysayan99@gmail.com>
2019-05-31 23:38:23 +0530

    X00T: Add boost props
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -200,7 +200,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60
+    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+    vendor.enable_prefetch=1 \
+    vendor.iop.enable_uxe=1 \
+    vendor.perf.iop_v3.enable=1 \
+    vendor.iop.enable_prefetch_ofr=1 \
+    persist.vendor.qti.games.gt.prof=1 \
+    vendor.perf.gestureflingboost.enable=true
+
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
