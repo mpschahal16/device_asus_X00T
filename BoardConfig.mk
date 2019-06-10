322085008621529a25ed9ee8d190d28a4ffeac71
Genkzsz11 <genkzsz11@gmail.com>
2019-06-10 13:51:51 +0700

    X00T: build BOARD_SEPOLICY private
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -281,6 +281,7 @@ VENDOR_SECURITY_PATCH := 2019-05-01
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Strip debug
